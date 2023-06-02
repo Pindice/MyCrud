41af52c4fa7e99c37da8615167cec1d44485fa4e
Pindice <pascal.indice@gmail.com>
2023-06-02 15:18:35 +0400

    create superuser + model update

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     gender = models.CharField(max_length=10)
