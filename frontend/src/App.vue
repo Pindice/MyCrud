83a1beea6dcb0f7f6cad1d7a074e65360fbfbe1e
Pindice <pascal.indice@gmail.com>
2023-06-12 16:20:16 +0400

    Update visuel

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,119 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
- 
   <div id="app">
-    <router-view></router-view>
-  </div>
+    <header class="header">
+      <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
+        <div class="container">
+          <a class="navbar-brand" href="#">Nom de l'entreprise</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" @click="toggleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" :class="{ 'show': isNavbarVisible }" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <router-link to="/home" class="nav-link">Home</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/person-list" class="nav-link">Person List</router-link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/info">Info</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
 
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      isNavbarVisible: false
+    };
+  },
+  methods: {
+    toggleNavbar() {
+      this.isNavbarVisible = !this.isNavbarVisible;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.header {
+  background-color: blue; /* Changez la couleur de fond selon vos préférences */
+  color: white;
+  padding: 10px;
+  width: 100%;
+}
+
+.navbar-brand {
+  color: white; /* Modifier la couleur */
+  font-size: 24px; /* Modifier la taille */
+}
+
+.navbar-toggler {
+  background-color: white;
+  border: none;
+  color: black;
+  font-size: 20px;
+  padding: 8px;
+  cursor: pointer;
+}
+
+.navbar-toggler:focus {
+  outline: none;
+}
+
+.navbar-toggler-icon {
+  display: inline-block;
+  color: white;
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: middle;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+  background-image: url(data:image/svg+xml;charset=utf-8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='black' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E);
+}
+
+.nav-link {
+  margin: 0 10px; /* Espacement horizontal entre les liens */
+  color: white !important;
+}
+
+.show {
+  display: block !important;
+}
+
+.navbar-nav > .nav-item.show {
+  display: block;
+}
+
+nav {
+  background-color: #f1f1f1;
+  padding: 10px;
+  margin-top: 10px;
+}
+
+nav ul {
+  list-style: none;
+  padding: 0;
+}
+
+nav li {
+  margin-bottom: 10px;
+}
+
+main {
+  margin-top: 60px; /* Ajustez la valeur selon la hauteur de l'entête */
 }
 </style>
--- frontend/src/components/PersonList.vue
+++ frontend/src/components/PersonList.vue
@@ -1,37 +1,68 @@
 <template>
-    <div>
-      <h1>Person List</h1>
-  
-      <!-- Formulaire pour ajouter ou mettre à jour une personne -->
-      <form @submit.prevent="submitForm">
-        <label>First Name:</label>
-        <input v-model="form.first_name" type="text" required>
-  
-        <label>Last Name:</label>
-        <input v-model="form.last_name" type="text" required>
-  
-        <label>Gender:</label>
-        <select v-model="form.gender" required>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="Other">Other</option>
-        </select>
-  
-        <label>Phone Number:</label>
-        <input v-model="form.phone_number" type="text" required>
-  
-        <label>Email:</label>
-        <input v-model="form.email" type="email" required>
-  
-        <label>Personal Email:</label>
-        <input v-model="form.email_personnal" type="email" required>
-  
-        <button v-if="!editing" type="submit">Add Person</button>
-        <button v-else type="submit">Update Person</button>
-        <button @click="cancelEdit" v-if="editing">Cancel</button>
+  <div>
+    <h1 class="text-center display-4">Person List</h1>
+
+    <div class="container">
+      <form @submit.prevent="submitForm" class="person-form">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="form-group">
+              <label>First Name:</label>
+              <input v-model="form.first_name" type="text" class="form-control" required>
+            </div>
+          </div>
+
+          <div class="col-sm-6">
+            <div class="form-group">
+              <label>Last Name:</label>
+              <input v-model="form.last_name" type="text" class="form-control" required>
+            </div>
+          </div>
+
+          <div class="col-sm-6">
+            <div class="form-group">
+              <label>Gender:</label>
+              <select v-model="form.gender" class="form-control" required>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="col-sm-6">
+            <div class="form-group">
+              <label>Phone Number:</label>
+              <input v-model="form.phone_number" type="text" class="form-control" required>
+            </div>
+          </div>
+
+          <div class="col-sm-12">
+            <div class="form-group">
+              <label>Email:</label>
+              <input v-model="form.email" type="email" class="form-control" required>
+            </div>
+          </div>
+
+          <div class="col-sm-12">
+            <div class="form-group">
+              <label>Personal Email:</label>
+              <input v-model="form.email_personnal" type="email" class="form-control" required>
+            </div>
+          </div>
+
+          <div class="col-sm-12">
+            <div class="form-group">
+              <button v-if="!editing" type="submit" class="btn btn-success">Add Person</button>
+              <button v-else type="submit" class="btn btn-primary">Update Person</button>
+              <button @click="cancelEdit" v-if="editing" class="red-button">Cancel</button>
+            </div>
+          </div>
+        </div>
       </form>
-  
-      <table>
+
+    <div class="table-responsive mx-auto">
+      <table class="table">
         <thead>
           <tr>
             <th>ID</th>
@@ -45,7 +76,7 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="person in persons" :key="person.id">
+          <tr v-for="person in persons" :key="person.id" :class="{'odd-row': person.id % 2 !== 0}">
             <td>{{ person.id }}</td>
             <td>{{ person.first_name }}</td>
             <td>{{ person.last_name }}</td>
@@ -54,121 +85,305 @@
             <td>{{ person.email }}</td>
             <td>{{ person.email_personnal }}</td>
             <td>
-              <button @click="editPerson(person)">Edit</button>
-              <button @click="deletePerson(person.id)">Delete</button>
+              <button @click="editPerson(person)" class="btn btn-primary">
+                Edit <i class="fas fa-edit"></i>
+              </button>
+              <button @click="showDeleteConfirmation(person.id)" class="red-button">
+                Delete <i class="fas fa-trash-alt"></i>
+              </button>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        form: {
-          id: null,
-          first_name: '',
-          last_name: '',
-          gender: '',
-          phone_number: '',
-          email: '',
-          email_personnal: ''
-        },
-        persons: [],
-        editing: false
-      };
-    },
-    methods: {
-      async submitForm() {
-        if (this.editing) {
-          await this.updatePerson();
-        } else {
-          await this.addPerson();
-        }
+
+      <div v-if="notification" class="notification alert alert-info">
+        {{ notification }}
+      </div>
+
+      <div v-if="showConfirmation" class="confirmation-modal">
+        <div class="modal-content">
+          <p>Are you sure you want to delete this person?</p>
+          <div class="modal-actions">
+            <button @click="deleteConfirmed" class="green-button">Yes</button>
+            <button @click="deleteCancelled" class="red-button">No</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      form: {
+        id: null,
+        first_name: '',
+        last_name: '',
+        gender: '',
+        phone_number: '',
+        email: '',
+        email_personnal: ''
       },
-      async addPerson() {
-        try {
-          const response = await axios.post('http://localhost:8000/persons/', this.form);
-          const createdPerson = response.data;
-          this.persons.push(createdPerson);
-          this.clearForm();
-        } catch (error) {
-          console.error(error.response);
-        }
-      },
-      async updatePerson() {
-        try {
-          const url = `http://localhost:8000/persons/${this.form.id}/`;
-          await axios.put(url, this.form);
-          const updatedPersonIndex = this.persons.findIndex(person => person.id === this.form.id);
-          this.$set(this.persons, updatedPersonIndex, this.form);
-          this.clearForm();
-          this.editing = false;
-        } catch (error) {
-          console.error(error.response);
-        }
-      },
-      async editPerson(person) {
-        this.form = { ...person };
-        this.editing = true;
-      },
-      async deletePerson(personId) {
-        try {
-          const url = `http://localhost:8000/persons/${personId}/`;
-          await axios.delete(url);
-          this.persons = this.persons.filter(person => person.id !== personId);
-        } catch (error) {
-          console.error(error.response);
-        }
-      },
-      async fetchPersons() {
-        try {
-          const response = await axios.get('http://localhost:8000/persons/');
-          this.persons = response.data;
-        } catch (error) {
-          console.error(error.response);
-        }
-      },
-      cancelEdit() {
-        this.clearForm();
-        this.editing = false;
-      },
-      clearForm() {
-        this.form = {
-          id: null,
-          first_name: '',
-          last_name: '',
-          gender: '',
-          phone_number: '',
-          email: '',
-          email_personnal: ''
-        };
+      persons: [],
+      editing: false,
+      notification: '',
+      showConfirmation: false,
+      personToDeleteId: null,
+    };
+  },
+  methods: {
+    async submitForm() {
+      if (this.editing) {
+        await this.updatePerson();
+      } else {
+        await this.addPerson();
       }
     },
-    mounted() {
-      this.fetchPersons();
+    async addPerson() {
+      try {
+        const response = await axios.post('http://localhost:8000/persons/', this.form);
+        const createdPerson = response.data;
+        this.persons.push(createdPerson);
+        this.clearForm();
+        this.showNotification('Person added successfully.');
+      } catch (error) {
+        console.error(error.response);
+        this.showNotification('Failed to add person. Please try again.', 'error');
+      }
+    },
+    async updatePerson() {
+      try {
+        const url = `http://localhost:8000/persons/${this.form.id}/`;
+        await axios.put(url, this.form);
+        const updatedPersonIndex = this.persons.findIndex(person => person.id === this.form.id);
+        this.$set(this.persons, updatedPersonIndex, this.form);
+        this.clearForm();
+        this.editing = false;
+        this.showNotification('Person updated successfully.');
+      } catch (error) {
+        console.error(error.response);
+        this.showNotification('Failed to update person. Please try again.', 'error');
+      }
+    },
+    async editPerson(person) {
+      this.form = { ...person };
+      this.editing = true;
+    },
+    async deletePerson(personId) {
+      this.personToDeleteId = personId;
+      this.showConfirmation = true;
+      try {
+        const url = `http://localhost:8000/persons/${personId}/`;
+        await axios.delete(url);
+        this.persons = this.persons.filter(person => person.id !== personId);
+        this.showNotification('Person deleted successfully.');
+      } catch (error) {
+        console.error(error.response);
+        this.showNotification('Failed to delete person. Please try again.', 'error');
+      }
+    },
+    async fetchPersons() {
+      try {
+        const response = await axios.get('http://localhost:8000/persons/');
+        this.persons = response.data;
+      } catch (error) {
+        console.error(error.response);
+        this.showNotification('Failed to fetch persons. Please try again.', 'error');
+      }
+    },
+    cancelEdit() {
+      this.clearForm();
+      this.editing = false;
+    },
+    clearForm() {
+      this.form = {
+        id: null,
+        first_name: '',
+        last_name: '',
+        gender: '',
+        phone_number: '',
+        email: '',
+        email_personnal: ''
+      };
+    },
+    showNotification(message) {
+      this.notification = message;
+      setTimeout(() => {
+        this.notification = '';
+      }, 3000);
+    },
+
+    showDeleteConfirmation(personId) {
+      this.personToDeleteId = personId;
+      this.showConfirmation = true;
+    },
+
+    deleteConfirmed() {
+      const personId = this.personToDeleteId;
+      const url = `http://localhost:8000/persons/${personId}/`;
+      axios
+        .delete(url)
+        .then(() => {
+          this.persons = this.persons.filter(person => person.id !== personId);
+          this.showNotification('Person deleted successfully.');
+        })
+        .catch(error => {
+          console.error(error.response);
+          this.showNotification('Failed to delete person. Please try again.', 'error');
+        });
+
+      this.showConfirmation = false;
+    },
+
+    deleteCancelled() {
+      // Annuler la suppression
+      this.showNotification('Deletion cancelled.');
+
+      // Fermer la boîte de dialogue modale
+      this.showConfirmation = false;
     }
-  };
-  </script>
-  
-  <style scoped>
-  table {
-    width: 100%;
-    border-collapse: collapse;
+  },
+  mounted() {
+    this.fetchPersons();
   }
-  
-  th, td {
-    padding: 8px;
-    text-align: left;
-    border-bottom: 1px solid #ddd;
-  }
-  
-  button {
-    margin-right: 5px;
-  }
-  </style>
-  
+};
+</script>
+
+
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+
+th {
+  font-weight: bold;
+}
+
+tr.odd-row {
+  background-color: #f5f5f5;
+}
+
+button {
+  margin-right: 5px;
+  padding: 8px 12px;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 14px;
+  transition: background-color 0.3s ease;
+}
+
+.red-button {
+  margin-right: 5px;
+  padding: 8px 12px;
+  background-color: red;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 14px;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: #45a049;
+}
+
+button:active {
+  background-color: #3c893d;
+}
+
+.notification {
+  background-color: #f1f1f1;
+  padding: 10px;
+  text-align: center;
+  margin-top: 10px;
+  border-radius: 5px;
+  color: #333;
+}
+
+.person-form {
+  background-color: #f9f9f9;
+  margin-top: 20px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.form-row {
+  margin-bottom: 10px;
+}
+
+.form-row label {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
+
+.form-row input,
+.form-row select {
+  width: 100%;
+  padding: 5px;
+  border: 1px solid #ddd;
+  border-radius: 3px;
+}
+
+.form-actions {
+  margin-top: 10px;
+}
+
+.fa-edit,
+.fa-trash-alt {
+  color: #333;
+}
+
+.fa-edit:hover,
+.fa-trash-alt:hover {
+  color: #ff0000;
+}
+
+.fa-edit {
+  margin-right: 5px;
+}
+
+.fa-trash-alt {
+  margin-left: 5px;
+}
+
+.confirmation-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border-radius: 4px;
+  max-width: 400px;
+  text-align: center;
+}
+
+.modal-actions {
+  margin-top: 20px;
+}
+</style>
