d9b3722689419f4c51f1d029222ca87ca2b4ae0c
Pindice <pascal.indice@gmail.com>
2023-06-18 20:32:28 +0400

    update django_heroku settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from django import django_heroku
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
